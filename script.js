59773116dc4cfb9cf2ef9c75791ae2398dc19ec7
Evaketesdi <evahollo85@yahoo.com>
2024-03-31 21:48:20 +0300

    menu overlay added

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ document.addEventListener("DOMContentLoaded", function () {
     textElement.style.transform = "translateY(0)";
   }, 500);
 });
+
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const menuOverlay = document.getElementById("menu-overlay");
+
+hamburgerMenu.addEventListener("click", function () {
+  this.classList.toggle("active");
+  menuOverlay.classList.toggle("active");
+
+  if (this.classList.contains("active")) {
+    this.parentNode.classList.add("no-shadow");
+  } else {
+    this.parentNode.classList.remove("no-shadow");
+  }
+});
