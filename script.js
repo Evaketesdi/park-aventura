de35a20239bf058d015b0d5132719042393e2c54
Evaketesdi <evahollo85@yahoo.com>
2024-04-14 21:51:04 +0300

    test

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const textElement = document.getElementById("animated-text");
 
-  // Delay the animation for a few milliseconds
   setTimeout(function () {
     textElement.style.opacity = "1";
     textElement.style.transform = "translateY(0)";
-  }, 500);
+  }, 1500);
 });
 
 const hamburgerMenu = document.getElementById("hamburger-menu");
